2dc691d88cb27bd33829db7a418c789858654ed0
denialofservice404 <faustas.litvinaitis@gmail.com>
2023-04-10 20:10:13 +0100

    Added order screen for Mario and fixed a bug with drinks being on pizza list

--- app.py
+++ app.py
@@ -6,14 +6,20 @@ import json
 app = Flask(__name__)
 
 orderList = []
+drinkList = ['Coca Cola (0.33L)', 'Fernandes (0.33L)', 'Fanta (0.33L)', 'Coca Cola Cherry (0.33L)', 'Amstel Radler 0.0 (0.33L)', 'Heineken 0.0 (0.33L)']
 
 @app.route('/')
-def Main_Page():
+def main_Page():
     return render_template('index.html')
+
+@app.route('/order_menu')
+def order_Page():
+    return render_template('orderscreen.html')
 
 @app.route('/oven', methods = ['POST'])
 def arduino_contact():
     global orderList
+
     if request.get_json() == 'check_list':
         print("Connected ", orderList)
         if len(orderList) >= 1:
@@ -23,22 +29,24 @@ def arduino_contact():
             return json.dumps(False)        
     
     elif request.get_json() == 'pizza_done':
-
-        for i in range(len(orderList) - 1):
-            orderList[i] = orderList[i + 1]
-
-        orderList = orderList[:-1]
-        return json.dumps('received')
+        
+        orderList.pop(0)
         print(orderList)
+        return json.dumps('received')
 
         
 
 @app.route('/order_data', methods = ['POST'])
 def order_data_proccessing():
-    global orderList
+    global orderList, drinkList
 
     receivedData = request.get_json()
     orderList.extend(receivedData['filteredOrderList'])
+
+    while any(whileItem in orderList for whileItem in drinkList):
+        for item in orderList:
+            if item in drinkList:
+                orderList.pop(orderList.index(item))
     
     print(orderList)
     return json.dumps('Received')
